ee3c14802fccc86ff53136f1380ac399806afc51
Bognaum <bognaum@gmail.com>
2021-07-26 09:34:53 +0300

    Added default actions.

--- src/extension.ts
+++ src/extension.ts
@@ -4,12 +4,15 @@ export function activate(context: vsc.ExtensionContext) {
 	context.subscriptions.push(... [
 		vsc.commands.registerCommand("bn-smart-clipboard.cut", () => {
 			vsc.window.showInformationMessage("bn-smart-clipboard.cut");
+			vsc.commands.executeCommand("editor.action.clipboardCutAction");
 		}),
 		vsc.commands.registerCommand("bn-smart-clipboard.copy", () => {
 			vsc.window.showInformationMessage("bn-smart-clipboard.copy");
+			vsc.commands.executeCommand("editor.action.clipboardCopyAction");
 		}),
 		vsc.commands.registerCommand("bn-smart-clipboard.paste", () => {
 			vsc.window.showInformationMessage("bn-smart-clipboard.paste");
+			vsc.commands.executeCommand("editor.action.clipboardPasteAction");
 		}),
 		vsc.commands.registerCommand("bn-smart-clipboard.openHistory", () => {
 			vsc.window.showInformationMessage("bn-smart-clipboard.openHistory");
