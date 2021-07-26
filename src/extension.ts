3300441d8ae95c109fc9595f01ab6578acc2edf7
Bognaum <bognaum@gmail.com>
2021-07-26 09:30:40 +0300

    Added testing messages.

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,18 @@ import * as vsc from 'vscode';
 
 export function activate(context: vsc.ExtensionContext) {
 	context.subscriptions.push(... [
-		vsc.commands.registerCommand("bn-smart-clipboard.cut", () => {}),
-		vsc.commands.registerCommand("bn-smart-clipboard.copy", () => {}),
-		vsc.commands.registerCommand("bn-smart-clipboard.paste", () => {}),
-		vsc.commands.registerCommand("bn-smart-clipboard.openHistory", () => {}),
+		vsc.commands.registerCommand("bn-smart-clipboard.cut", () => {
+			vsc.window.showInformationMessage("bn-smart-clipboard.cut");
+		}),
+		vsc.commands.registerCommand("bn-smart-clipboard.copy", () => {
+			vsc.window.showInformationMessage("bn-smart-clipboard.copy");
+		}),
+		vsc.commands.registerCommand("bn-smart-clipboard.paste", () => {
+			vsc.window.showInformationMessage("bn-smart-clipboard.paste");
+		}),
+		vsc.commands.registerCommand("bn-smart-clipboard.openHistory", () => {
+			vsc.window.showInformationMessage("bn-smart-clipboard.openHistory");
+		}),
 	]);
 }
 
